ec8461ad430a0271aef58c3ac2211be4f984ce8a
ShadowAya <jonhlad@gmail.com>
2024-02-28 19:16:14 +0100

    add backout

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 export type Config = {
   type: string;
 } & {
@@ -5,7 +6,7 @@ export type Config = {
   offset?: number;
   negative_margin?: number;
   timeout?: number;
-  strict_url_change?: boolean;
   disable_in_edit_mode?: boolean;
   remove_anchor?: boolean;
+  backout?: boolean;
 }
